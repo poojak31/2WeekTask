b77b8be635f1e775c16e392780f2fb235a2df16f
Pooja <pooja@Pooja-Desktop.local>
2020-02-25 14:43:20 +0530

    change

--- Week-2/Array.js
+++ Week-2/Array.js
@@ -1,84 +1,84 @@
 const Numbers=new Array();
 
-   function insertNumbers()
-  {
-     for(let i=0;i<=100;i++)
-    {
-       Numbers.push(i);
-    }
-     console.log(Numbers);
-  }
+function insertNumbers()
+{
+    for(let i=0;i<=100;i++)
+{
+    Numbers.push(i);
+}
+    console.log(Numbers);
+}
 
-    function displayContents()
+function displayContents()
+{
+    for(let i=0;i<Numbers.length;i++)
     {
-      for(let i=0;i<Numbers.length;i++)
-       {
-          console.log(Numbers[i]);
-       }
+        console.log(Numbers[i]);
     }
+}
 
-    function checkArray()
+function checkArray()
 {  
-        for(let i=0;i<=100;i++)
-  {
-       if(isFizzBuzz(i))
-       {
-         console.log("Fizz Buzz")
-       }
-      else if(isFizz(i))
-       {
-         console.log("Fizz")
-       }
-      else if(isBuzz(i))
-       {
-        console.log("Buzz")
-       }
-      else
-       {
-         console.log(i)
-       }
-  }
+    for(let i=0;i<=100;i++)
+{
+    if(isFizzBuzz(i))
+    {
+        console.log("Fizz Buzz")
+    }
+    else if(isFizz(i))
+    {
+        console.log("Fizz")
+    }
+    else if(isBuzz(i))
+    {
+    console.log("Buzz")
+    }
+    else
+    {
+        console.log(i)
+    }
+}
 }
 
 
-     function  isFizzBuzz(i)
-   {
-     if(i%3===0 && i%5===0)
-      {
-        return true;
-      }
-     else
-      {
-         return false;
-      }
-   }
+    function  isFizzBuzz(i)
+{
+    if(i%3===0 && i%5===0)
+    {
+    return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 
-     function  isFizz(i)
-   {
-     if(i%3===0)
-      {
-        return true;
-      }
-     else
-      {
-         return false;
-      }
-   }
+    function  isFizz(i)
+{
+    if(i%3===0)
+    {
+    return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 
 
-   function  isBuzz(i)
-   {
-     if(i%5===0)
-      {
-        return true;
-      }
-     else
-      {
-         return false;
-      }
-   }
+function  isBuzz(i)
+{
+    if(i%5===0)
+    {
+    return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 
 
