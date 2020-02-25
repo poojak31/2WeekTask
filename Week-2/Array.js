fbf52cd3ed0313759995eb3cde9ba35f15f8d8cf
Pooja <pooja@Pooja-Desktop.local>
2020-02-25 10:59:12 +0530

    change

--- Week-2/Array.js
+++ Week-2/Array.js
@@ -2,20 +2,20 @@ var Numbers=new Array();
 
 function insertNumbersIntoArray()
 {
- for(let i=0;i<=100;i++)
- {
-    Numbers.push(i);
- }
-   console.log(Numbers);
+for(let i=0;i<=100;i++)
+{
+Numbers.push(i);
+}
+console.log(Numbers);
 }
 
 function displayContentsOfTheArray()
 {
-    
-    for(let i=0;i<Numbers.length;i++)
-    {
-        console.log(Numbers[i]);
-    }
+
+for(let i=0;i<Numbers.length;i++)
+{
+    console.log(Numbers[i]);
+}
 
 }
 
