f62a64edc1cb77f2b6357fc86c4604d7b4aabe2b
Pooja <pooja@Pooja-Desktop.local>
2020-02-25 10:02:58 +0530

    first commit

---
+++ Week-2/Array.js
@@ -0,0 +1,26 @@
+var Number=new Array();
+
+function InsertNumberIntoArray()
+{
+for(let i=0;i<=100;i++)
+{
+    Number.push(i);
+}
+    console.log(Number);
+}
+
+function DisplayContentsOfTheArray()
+{
+    for(let i=0;i<=100;i++)
+    {
+        Number.push(i);
+    }
+
+    for(let i=0;i<Number.length;i++)
+    {
+        console.log(Number[i]);
+    }
+
+    
+}
+
