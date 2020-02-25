dca0814e876e92ad6967c484045847fe8c76ccdc
Pooja <pooja@Pooja-Desktop.local>
2020-02-25 12:24:43 +0530

    new chnages

--- Week-2/Array.js
+++ Week-2/Array.js
@@ -1,6 +1,6 @@
 var Numbers=new Array();
 
-function insertNumbersIntoArray()
+function insertNumbersInArray()
 {
 for(let i=0;i<=100;i++)
 {
@@ -9,13 +9,35 @@ Numbers.push(i);
 console.log(Numbers);
 }
 
-function displayContentsOfTheArray()
+function displayContentsOfArray()
 {
 
 for(let i=0;i<Numbers.length;i++)
 {
-    console.log(Numbers[i]);
+console.log(Numbers[i]);
 }
 
+}
+
+function checkArray(Numbers)
+{
+for(let i=0;i<=Numbers;i++)
+{
+if(i%3===0 && i%5===0)
+{
+console.log("Fizz Buzz");
+}
+else if(i%3===0)
+{
+console.log("Fizz");
+}
+else if(i%5===0)
+{
+console.log("Buzz");
+}
+else{
+console.log(i);
+}  
+}
 }
 
